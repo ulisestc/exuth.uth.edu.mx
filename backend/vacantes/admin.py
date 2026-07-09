6e2c6171a0db52e5b76ad60de0b42e20819ebc63
ulisestc <ulisestc27@gmail.com>
2026-07-08 20:13:27 -0600

    feat(vacantes): implementación de los serializadores de Vacante y RequisitoIdioma

--- backend/vacantes/admin.py
+++ backend/vacantes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vacante, RequisitoIdioma
 
 # Register your models here.
+admin.site.register(Vacante)
+admin.site.register(RequisitoIdioma)
---
+++ backend/vacantes/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from django.db import transaction
+from .models import Vacante, RequisitoIdioma
+from core.models import Idioma
+
+class RequisitoIdiomaSerializer(serializers.ModelSerializer):
+    #definir como se recibe el id del idioma
+    idioma = serializers.PrimaryKeyRelatedField(queryset=Idioma.objects.all())
+
+    class Meta:
+        model = RequisitoIdioma
+        fields = ['idioma', 'nivel', 'obligatorio']
+
+class VacanteSerializer(serializers.ModelSerializer):
+    #anidar serializer de RequisitoIdioma
+    idiomas = RequisitoIdiomaSerializer(many=True)
+
+    class Meta:
+        model = Vacante
+        fields = '__all__'
+
+    def validate(self, attrs):
+        sueldo_min = attrs.get('sueldo_minimo')
+        sueldo_max = attrs.get('sueldo_maximo')
+        if sueldo_min is not None and sueldo_max is not None and sueldo_min > sueldo_max:
+            raise serializers.ValidationError("El sueldo mínimo no puede ser mayor que el sueldo máximo.")
+        return attrs
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        idiomas_data = validated_data.pop('idiomas', [])
+        vacante = Vacante.objects.create(**validated_data)
+        for idioma_data in idiomas_data:
+            RequisitoIdioma.objects.create(vacante=vacante, **idioma_data)
+        return vacante
+    
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        #extraer idiomas
+        idiomas_data = validated_data.pop('idiomas', None)
+
+        # 2. Actualizar los campos propios de la Vacante
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+    
+        # 3. Guardar los cambios de la instancia principal en la base de datos
+        instance.save()
+        
+        # 4. Manejar los idiomas (Hard Reset)
+        if idiomas_data is not None:
+            RequisitoIdioma.objects.filter(vacante=instance).delete()  # Eliminar todos los idiomas existentes
+            for idioma_data in idiomas_data:
+                RequisitoIdioma.objects.create(vacante=instance, **idioma_data)  # Crear nuevos idiomas
+        return instance
