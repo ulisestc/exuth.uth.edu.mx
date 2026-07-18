1159aece2fff87030ab7afe5becd3283ddb803bb
ulisestc <ulisestc27@gmail.com>
2026-07-17 20:40:53 -0600

    feat(vacantes): agregar permiso IsEmpresaAuthorOrReadOnly a Vacantes Viewset y sus pruebas correspondientes

--- backend/vacantes/views.py
+++ backend/vacantes/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, permissions
 from .models import Vacante
 from .serializers import VacanteSerializer
+from .permissions import IsEmpresaAuthorOrReadOnly
 
 class VacanteViewSet(viewsets.ModelViewSet):
     queryset = Vacante.objects.all()
     serializer_class = VacanteSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsEmpresaAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         # Asignar la empresa del usuario autenticado al crear una vacante
